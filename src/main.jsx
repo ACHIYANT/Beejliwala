3e60984528c63efcb334f5a060efa300123f6b59
ACHIYANT <achiyant@gmail.com>
2025-02-06 14:23:59 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import SplashScreen from "./components/splashScreen.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
